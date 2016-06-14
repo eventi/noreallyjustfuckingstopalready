9f2c8e590470719f95cad78d8a04caecd9d895af
Joe Block <jpb@unixorn.net>
2016-06-14 14:56:54 -0700

    Add zsh plugin file
    Rather than having to keep checking back to see if there are updates,
    make the repo loadable by antigen/oh-my-zsh/zgen. Antigen and Zgen are
    both smart enough so that you can have them automatically check for
    updates periodically so you get the updated version when Apple
    inevitably breaks things in a future OS release

---
+++ noreallyjustfuckingstopalready.plugin.zsh
@@ -0,0 +1,9 @@
+# Do plugin setup to make this easily loadable by oh-my-zsh, zgen and other
+# oh-my-zsh-compatible frameworks
+
+if [[ "$(uname -s)" = "Darwin" ]]; then
+  # Add the plugin's diretory to user's path
+  PLUGIN_HOME="$(dirname $0)"
+  export PATH=${PATH}:${PLUGIN_HOME}
+  alias flush-osx-cache=flu.sh
+fi
