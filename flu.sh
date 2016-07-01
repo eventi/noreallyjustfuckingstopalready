3a37523bba539b2e917e9cef88b8091b5e88d065
Turtle Kalus <turtle@nuna.com>
2016-06-30 17:13:17 -0700

    Add Support for OSX 10.11
    - Add comments for each section
    - Increment version to 0.0.2 (behavior change)

--- flu.sh
+++ flu.sh
@@ -5,16 +5,24 @@ LONGVERSION=$(sw_vers -productVersion)
 MINOR=$(echo $LONGVERSION | cut -d. -f2)
 UPDATE=$(echo $LONGVERSION | cut -d. -f3)
 
-if (( $MINOR < 7 )) ; then 
+if (( $MINOR < 7 )) ; then
+    # BeginingOfTime-10.6
     FLUSH="dscacheutil -flushcache"
 elif (( $MINOR < 10 )) ; then
+    # 10.7-10.9
     FLUSH="killall -HUP mDNSResponder"
-elif (( $MINOR == 10 )) ; then
+elif (( $MINOR < 11 )) ; then
+    # 10.10
     if (( $UPDATE < 4 )) ; then
+        # 10.10.0-10.10.3
         FLUSH="discoveryutil mdnsflushcache"
     else
+        # 10.10.4+
         FLUSH="killall -HUP mDNSResponder"
     fi
+elif (( $MINOR == 11 )) ; then
+    # 10.11
+    FLUSH="dscacheutil -flushcache"
 else
     echo VERSION $LONGVERSION
     FLUSH="killall -HUP mDNSResponder"
